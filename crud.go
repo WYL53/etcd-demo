1710a67b5ba7191cfb330af181b9c2a8003dfb63
843794313@qq.com <wuyl>
2020-10-19 17:40:43 +0800

    init

---
+++ crud.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"github.com/coreos/go-etcd/etcd"
+)
+
+func main()  {
+	machines := []string{"http://127.0.0.1:2379"}
+    client := etcd.NewClient(machines)
+
+    if _, err := client.Set("/foo", "bar", 0); err != nil {
+        log.Fatal(err)
+    }
+}
