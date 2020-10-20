36b0afbed087e9e456c703d1a3a8ab714613f08c
843794313@qq.com <wuyl>
2020-10-20 16:52:58 +0800

    添加版本控制

--- crud.go
+++ crud.go
@@ -1,14 +1,59 @@
 package main
 
-import(
-	"github.com/coreos/go-etcd/etcd"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
 )
 
-func main()  {
-	machines := []string{"http://127.0.0.1:2379"}
-    client := etcd.NewClient(machines)
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"118.89.22.227:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		// handle error!
+		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		return
+	}
 
-    if _, err := client.Set("/foo", "bar", 0); err != nil {
-        log.Fatal(err)
-    }
+	fmt.Println("connect to etcd success")
+	defer cli.Close()
+
+	// put
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	Key := "q1mi"
+	_, err = cli.Put(ctx, Key, "dsb")
+	cancel()
+	if err != nil {
+		fmt.Printf("put to etcd failed, err:%v\n", err)
+		return
+	}
+	// get
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	resp, err := cli.Get(ctx, Key)
+	cancel()
+	if err != nil {
+		fmt.Printf("get from etcd failed, err:%v\n", err)
+		return
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s:%s\n", ev.Key, ev.Value)
+	}
+
+	//del
+	fmt.Println("start delete")
+	delResp, err := cli.Delete(context.TODO(), Key, clientv3.WithPrevKV())
+	if err != nil {
+		fmt.Printf("del key  failed , err： %v \n", err)
+		return
+	}
+	if delResp.Deleted > 0 {
+		for _, kv := range delResp.PrevKvs {
+			fmt.Println(string(kv.Key), string(kv.Value))
+		}
+	}
+
 }
